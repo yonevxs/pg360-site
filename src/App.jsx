a92fa9041c979f8e2c63e6c4a51dcf74290c3d0c
Gustavo Lemos  Oliveira <gugaprogramador24@gmail.com>
2025-11-11 23:50:43 -0300

    Revert "Develop"

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
-=======
-// src/App.jsx
-
-import React from 'react';
-import Navbar from './components/Navbar.jsx';
-import Home from './components/Home.jsx';
-import Carrossel from './components/Carrossel.jsx';
->>>>>>> 7e8ed6eb4f548b2c31922987864dccf5c7ce6016
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
     <div className="w-full min-h-screen">
       <NavBar/>
     </div>
   )
-=======
-      <div>
-      <Home />
-      <Carrossel/>
-      
-
-    </div>
-  );
->>>>>>> 7e8ed6eb4f548b2c31922987864dccf5c7ce6016
 }
 
 export default App
