1a03435296b9d755c97be85e6c2e775618c9f8f0
GustavoinCode24 <gugaprogramador24@gmail.com>
2025-11-15 21:31:37 -0300

    subindo o cors

--- src/main/java/br/com/pg360/api/controller/AvaliacaoController.java
+++ src/main/java/br/com/pg360/api/controller/AvaliacaoController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173"})
 @RequestMapping("/avaliacoes")
 public class AvaliacaoController {
 
